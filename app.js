dd9f33db62e4e6c60d816a16df2e3ee00e81075b
Oranqif <23008720@myrp.edu.sg>
2024-07-29 13:26:48 +0800

    help, i am at the end of my rope

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const multer = require('multer');
 const app = express();
-const port = process.env.PORT || 3306;
+let port = process.env.PORT || 0;
 
 app.listen(port, () => {
     console.log(`i am done with this shit but here it is ${port}`)
